b68a8d7afe5ff6586d210df39abc265e5b15f10c
Sl1van <58076574+Sl1van@users.noreply.github.com>
2021-12-14 08:33:50 +0100

    Added empty editActivity

---
+++ app/src/main/java/ch/zli/nfcsharer/activities/EditActivity.java
@@ -0,0 +1,18 @@
+package ch.zli.nfcsharer.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ch.zli.nfcsharer.R;
+
+public class EditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+    }
+
+
+}
